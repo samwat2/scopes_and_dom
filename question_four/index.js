d2a7b13991aa7cb60e8ca2803732ac9256dfa5b9
Sammy Watchman <swsw872@gmail.com>
2018-01-18 09:54:46 -0700

    fixes #4

---
+++ question_four/index.js
@@ -0,0 +1,54 @@
+//var correct = document.getElementById('password');
+//passcode.addEventListener('input', function(){
+
+//})
+//var correct = document.getElementById('password');
+//document.getElementById('username').length= "username"
+
+/*
+document.getElementById('password').value = 'keypress'
+
+
+
+function pwd(password) {
+	pwd.addEventListener()
+	if (answer === '12345'){
+		document.getElementById('title').innerText = CONGRATS
+	} else{
+		alert('please try again')
+	}var answer = 'password';
+	pwd(answer)
+}
+
+
+
+function usr(user_name) {
+	if ((answer === 14).length) {
+		document.getElementById('title').innerText = WELL_DONE
+	} else {
+		alert('plz review your shiz!')
+	}var username= 'user_name';
+	usr(answer)
+}
+
+
+var btn= document.getElementById('print');
+	btn.addEventListener('click',function(){
+	function (user_name, password);
+});
+
+/*
+var title= document.getElementById('user_name');
+	user_name.addEventListener('keyup', function (){
+		document.getElementById('title').innerText = this.value
+
+});
+*/
+document.addEventListener('submit', function(e){
+	e.preventDefault();
+	if (document.getElementById('password').value === '12345678' && document.getElementById('user_name').value.length < 14) {
+		document.getElementById('title').innerText = "Congrats!"
+	} else {
+		alert('please try again');
+	}
+});
